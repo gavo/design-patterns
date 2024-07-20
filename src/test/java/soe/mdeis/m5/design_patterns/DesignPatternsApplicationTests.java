3b1571342b6803e69bfffb71bfae5f1171541098
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-07-20 18:05:58 -0400

    initial commit

---
+++ src/test/java/soe/mdeis/m5/design_patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package soe.mdeis.m5.design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
