a29f847c2c0a8e83d7fc81808fc02ee2ca801bb3
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-07-24 15:49:54 -0400

    Adding show_users_v2 test

--- src/test/java/soe/mdeis/m5/design_patterns/UsersPanelTest.java
+++ src/test/java/soe/mdeis/m5/design_patterns/UsersPanelTest.java
@@ -1,12 +1,24 @@
 package soe.mdeis.m5.design_patterns;
 
 import org.junit.jupiter.api.Test;
+
+import soe.mdeis.m5.design_patterns.aggregate.FacebookAggregate;
+import soe.mdeis.m5.design_patterns.aggregate.GoogleAggregate;
 import soe.mdeis.m5.design_patterns.view.UsersPanel;
+import soe.mdeis.m5.design_patterns.view.UsersPanelV2;
 
 public class UsersPanelTest {
     @Test
-    void show_users_v1(){
+    void show_users_v1() {
         var panel = new UsersPanel();
+        panel.show();
+    }
+
+    @Test
+    void show_users_v2() {
+        var panel = new UsersPanelV2(new FacebookAggregate());
+        panel.addAggregate(new GoogleAggregate());
+
         panel.show();
     }
 
